576f9fba9b85a19b69f63f26e106cd4b8a059ee3
Didhi Samiksha <didhisamiksha@gmail.com>
2026-07-27 14:07:07 +0530

    Initial Flask application setup

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return """
+    <h1>CI/CD Pipeline Project</h1>
+    <p>Application is running successfully!</p>
+    """
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
